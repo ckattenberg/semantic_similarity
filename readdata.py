0ee292ade373121ee27abd0b67abf11e4f7b6622
C. Kattenberg <celine.kattenberg@student.uva.nl>
2021-01-05 16:49:25 +0100

    Add train-test split

--- readdata.py
+++ readdata.py
@@ -1,9 +1,11 @@
 import pandas
 import numpy
+from sklearn.model_selection import train_test_split
 
-def read():
+def read(test_percentage):
     data = pandas.read_csv("quora_duplicate_questions.tsv", converters={'question1': str, 'question2':str}, sep="\t")
-    return data
+    train, test = train_test_split(data, test_size=test_percentage)
+    return(train, test)
 
 if __name__ == "__main__":
     print(read().dtypes)
