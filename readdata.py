b5399a7d55767dfe9d441c2dfdab0c89505575b2
C. Kattenberg <celine.kattenberg@student.uva.nl>
2021-01-05 16:53:27 +0100

    Remove train/test split

--- readdata.py
+++ readdata.py
@@ -2,10 +2,10 @@ import pandas
 import numpy
 from sklearn.model_selection import train_test_split
 
-def read(test_percentage):
+def read():
     data = pandas.read_csv("quora_duplicate_questions.tsv", converters={'question1': str, 'question2':str}, sep="\t")
-    train, test = train_test_split(data, test_size=test_percentage)
-    return(train, test)
+    training_data = data
+    return data
 
 if __name__ == "__main__":
     print(read().dtypes)
