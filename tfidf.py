81402e274d8871f7c2462b476a75a986e74c5789
C. Kattenberg <celine.kattenberg@student.uva.nl>
2021-01-05 17:56:06 +0100

    Train vectorizer on entire corpus

--- tfidf.py
+++ tfidf.py
@@ -6,7 +6,6 @@ from sklearn.metrics import accuracy_score
 
 
 def cosine_sim(text1, text2):
-   tfidf = vectorizer.fit_transform([text1, text2])
    return ((tfidf*tfidf.T).A)[0,1]
 
 vectorizer = TfidfVectorizer(min_df=1, stop_words='english')   
@@ -16,10 +15,16 @@ if __name__ == "__main__":
    correct = list(data.is_duplicate)[:5000]
    result = []
 
+   # Put all the sentences in the dataset in a large corpus list.
+   corpus = list(data['question1']) + list(data['question2'])
+
+   X = vectorizer.fit(corpus)
+
    for id in data['id']:
       text1 = data['question1'][id]
       text2 = data['question2'][id]
-      score = cosine_sim(text1,text2)
+      tfidf = X.transform([text1, text2])
+      score = cosine_sim(tfidf)
 
       # Threshold
       if score > 0.9:
