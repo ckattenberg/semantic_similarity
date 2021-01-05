7195784623f8b8e68e4dcd6aca365f746a24c26a
C. Kattenberg <celine.kattenberg@student.uva.nl>
2021-01-05 16:54:07 +0100

    Remove train/test split

--- readdata.py
+++ readdata.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 
 def read():
     data = pandas.read_csv("quora_duplicate_questions.tsv", converters={'question1': str, 'question2':str}, sep="\t")
-    training_data = data
     return data
 
 if __name__ == "__main__":
