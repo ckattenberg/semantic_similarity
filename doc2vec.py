81dbb2ee7960f4853fba0b8f106e8b6a1163c16a
C. Kattenberg <celine.kattenberg@student.uva.nl>
2021-01-06 19:22:50 +0100

    Integrate testing into this file

--- doc2vec.py
+++ doc2vec.py
@@ -9,7 +9,8 @@ def read_data():
 	train, test = preprocess.clean_split(data)
 	train_corpus = list(train['question1']) + list(train['question2'])
 	test_corpus = list(test['question1']) + list(test['question2'])
-	return(train_corpus, test_corpus)
+	true_duplicate = list(test['is_duplicate'])
+	return(train_corpus, test_corpus, true_duplicate)
 
 # Turn corpus into TaggedDocument (as input for model training)
 def read_corpus(corpus, tokens_only=False):
@@ -36,12 +37,40 @@ def doc2vec(model, sent):
 	vector = model.infer_vector(sent)
 	return(vector)
 
+def test(model_path, test_data, threshold=0.6):
+	model = load_model("doc2vec.model")
+	test1 = list(test['question1'])
+	test2 = list(test['question2'])
+	true_duplicate = read_data()
+
+	model_duplicate = []
+	dist_list = []
+
+	# For each pair of test sentences
+	for i in range(len(test1)):
+		# Get the vectors of both sentences
+		q1 = test1[i]
+		q2 = test2[i]
+
+		vec1 = model.doc2vec(q1)
+		vec2 = model.doc2vec(q2)
+
+		# Compare their cosine similarity
+		dist = distance.cosine(vec1, vec2)
+		dist_list.append(dist)
+
+		# Put into list
+		if(dist > threshold):
+			model_duplicate.append(1)
+		else:
+			model_duplicate.append(0)
+
 
 # Main function
 if __name__ == "__main__":
 
 	# Read in the data
-	train_corpus, test_corpus = read_data()
+	train_corpus, test_corpus, true_duplicate = read_data()
 	train_data = list(read_corpus(train_corpus))
 	test_data = list(read_corpus(test_corpus, tokens_only=True))
 
