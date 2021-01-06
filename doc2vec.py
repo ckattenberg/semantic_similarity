f955568c019842095b57ac3027b3073b52d219de
C. Kattenberg <celine.kattenberg@student.uva.nl>
2021-01-06 17:21:49 +0100

    Add read_corpus function

--- doc2vec.py
+++ doc2vec.py
@@ -1,6 +1,27 @@
 import os
 import gensim
+import readdata
+import preprocess
+
+def read_corpus(corpus):
+	for i, line in enumerate(corpus):
+		yield gensim.models.doc2vec.TaggedDocument(line, [i])
+
 
 # Main function
 if __name__ == "__main__":
-	pass
+
+	# Read in the data
+	data = readdata.read()[:3]
+	data = preprocess.clean_process(data)
+	corpus = list(data['question1']) + list(data['question2'])
+	tags = list(data['id'])
+
+	print(corpus)
+
+
+	train_corpus = list(read_corpus(corpus))
+
+	print(train_corpus)
+
+
