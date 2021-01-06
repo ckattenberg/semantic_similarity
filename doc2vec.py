22aeda0bbb6cf6cc237bfd5ac5c803cf640a4983
C. Kattenberg <celine.kattenberg@student.uva.nl>
2021-01-06 17:24:12 +0100

    Add read_data function

--- doc2vec.py
+++ doc2vec.py
@@ -3,25 +3,24 @@ import gensim
 import readdata
 import preprocess
 
+# Read in the data and preprocess it.
+def read_data():
+	data = readdata.read()[:3]
+	data = preprocess.clean_process(data)
+	corpus = list(data['question1']) + list(data['question2'])
+	return(corpus)
+
+# Turn corpus into TaggedDocument (as input for model training)
 def read_corpus(corpus):
 	for i, line in enumerate(corpus):
 		yield gensim.models.doc2vec.TaggedDocument(line, [i])
-
 
 # Main function
 if __name__ == "__main__":
 
 	# Read in the data
-	data = readdata.read()[:3]
-	data = preprocess.clean_process(data)
-	corpus = list(data['question1']) + list(data['question2'])
-	tags = list(data['id'])
-
-	print(corpus)
-
-
+	corpus = read_data()
 	train_corpus = list(read_corpus(corpus))
 
-	print(train_corpus)
 
 
