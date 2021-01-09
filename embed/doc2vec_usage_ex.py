31492bf51f95c7786606b2d1e612ce69b4fc0cdf
C. Kattenberg <celine.kattenberg@hotmail.com>
2021-01-09 18:23:37 +0100

    Update doc2vec_usage_ex.py

--- embed/doc2vec_usage_ex.py
+++ embed/doc2vec_usage_ex.py
@@ -1,15 +1,9 @@
 from . import doc2vec
 
-def read_data():
-	print("Reading in the data.")
-	data = readdata.read()
-	training_data = preprocess.clean_process(data)
-	# In gensim, a "corpus" is a list of documents.
-	training_corpus = list(training_data['question1']) + list(training_data['question2'])
-	return(training_corpus)
-	
 # Read in the data
-training_corpus = read_data()
+data = readdata.read()
+training_data = preprocess.clean_process(data)
+training_corpus = doc2vec.get_corpus(training_data)
 doc2vec.create_model(training_corpus, "doc2vec.model")
 model = doc2vec.load_model("doc2vec.model")
 embedding = doc2vec.doc2vec(model, ["The quick brown fox jumps over the lazy dog."])
