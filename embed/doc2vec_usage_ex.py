f8a84329c5fcf6076f8198ab13d4d5658eb137c4
C. Kattenberg <celine.kattenberg@hotmail.com>
2021-01-09 18:44:07 +0100

    Update doc2vec_usage_ex.py

--- embed/doc2vec_usage_ex.py
+++ embed/doc2vec_usage_ex.py
@@ -3,8 +3,7 @@ from . import doc2vec
 # Read in the data
 data = readdata.read()
 training_data = preprocess.clean_process(data)
-training_corpus = doc2vec.get_corpus(training_data)
-doc2vec.create_model(training_corpus, "doc2vec.model")
+doc2vec.create_model(training_data, "doc2vec.model")
 model = doc2vec.load_model("doc2vec.model")
 embedding = doc2vec.doc2vec(model, ["The quick brown fox jumps over the lazy dog."])
 
