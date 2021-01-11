e7ef568a94526ba000015083e67930389523a1c4
AnandaMaya <anandamayapradhan@gmail.com>
2021-01-11 20:45:08 +0100

    universal sentence encoder nn

--- run_use.py
+++ run_use.py
@@ -1,7 +1,13 @@
 from embed import universal_sentence_encoder as use
-from process import readdata
+from process import readdata, preprocess
 import pandas as pd
 import numpy as np
+from classifiers import binaryclassification as bc
+from keras.models import Sequential
+from keras.layers import Dense
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.metrics import accuracy_score
+import tensorflow as tf
 
 def embed_data(data):
     q1 = data["question1"]
@@ -27,10 +33,53 @@ def compare(sim, is_duplicate):
     print("Recall test: ", tp/(tp + fn))
     return tp, tf, fp, fn
 
+# baseline model
+def create_baseline():
+	# create model
+	model = Sequential()
+	model.add(Dense(1024, input_dim=1024, activation='relu'))
+	model.add(Dense(1, activation='sigmoid'))
+	# Compile model
+	model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+	return model
+
+def concat_col(em_df):
+    result = []
+    em_df.apply(lambda row: result.append(np.concatenate((row.em1, row.em2))), axis=1)
+    return np.array(result)
+
+
+def train_model(X_train, y_train, batch_size = 200):
+    estimator = KerasClassifier(build_fn=create_baseline, epochs=100, batch_size=batch_size, verbose=1)
+    # scaler = preprocessing.MinMaxScaler()
+    # X_train= scaler.fit_transform(X_train)
+    estimator.fit(X_train, y_train)
+    return estimator
 
 if __name__ == "__main__":
     data = readdata.read()
-    em_df = embed_data(data)
-    sims = sim_data(em_df)
-    dups = data["is_duplicate"]
-    compare(sims, dups)
+
+    # Split raw_data into train/test set
+    X_train, y_train, X_test, y_test = preprocess.split_train_test(data)
+    print('--- vectorizing data ---')
+    train_em = embed_data(X_train)
+    test_em = embed_data(X_test)
+    concat_em_train = concat_col(train_em)
+    concat_em_test = concat_col(test_em)
+    # data_train = pd.DataFrame({"train": concat_em_train})
+    # data_test = pd.DataFrame({"test": concat_em_test})
+
+    # Train model on training set
+    print('--- training model ---')
+    model = train_model(concat_em_train, y_train, 25)
+
+    # Test model on test set
+    print('--- testing model ---')
+    accuracy = bc.test_model(concat_em_test, y_test, model)
+
+    ''' Test Kfold '''
+    # X = np.concatenate((X_train_d2v_vectorized, X_test_d2v_vectorized))
+    # Y = np.append(y_train, y_test)
+    # print(bc.train_test_model_kfold(X,Y, batch_size = 200))
+
+    print('Accuracy: ', accuracy)
