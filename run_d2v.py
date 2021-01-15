240475b66e72431fcb23c12f826fab77f723ab3b
C. Kattenberg <celine.kattenberg@student.uva.nl>
2021-01-15 14:04:16 +0100

    Create models folder if it doesn't exist yet

--- run_d2v.py
+++ run_d2v.py
@@ -2,6 +2,7 @@
 from process import preprocess
 from process import readdata
 from embed import doc2vec as d2v
+import os
 
 if __name__ == "__main__":	
 
@@ -15,5 +16,10 @@ if __name__ == "__main__":
 		data = readdata.read()
 		print("Cleaning data")
 		training_data = preprocess.clean_process(data)
+
+		# Create a /models folder if it does not exist yet.
+		if not os.path.exists('models'):
+    		os.makedirs('models')
+
 		d2v.create_model(training_data, "models/doc2vec.model")
 		
