52f08f6de093d2bcd875e04c46b3d9f9d1250a56
C. Kattenberg <celine.kattenberg@student.uva.nl>
2021-01-18 16:00:43 +0100

    Change test function

--- run_test_d2v.py
+++ run_test_d2v.py
@@ -15,9 +15,12 @@ if __name__ == "__main__":
     ''' All models are located in classifiers/binaryclassification.py '''
     models = ['single_layer_200','double_layer_200']
 
-    ''' d2v '''
+     ''' d2v '''
     X_d2v_vectorized = doc2vec.vectorize_data_d2v(X, d2v_model)
-    results_d2v = bc.train_test_models(X_d2v_vectorized, Y, 'd2v', models, 200)
+    X_train, X_test, y_train, y_test = preprocess.split_train_test_vect(X_d2v_vectorized, Y)
+    model = bc.train_model(X_train, y_train, 'd2v', 200)
+    results_d2v = bc.test_model(X_test, y_test, model)
+    print(results_d2v)
 
     print(pd.DataFrame(data=results_d2v))
     ''' Results are automatically saved in results/ '''
