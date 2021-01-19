ee6839467222e734028fd76d3660cbb3a7b80378
stewiemcbacon <stefan.dijkstra@hotmail.com>
2021-01-20 00:03:13 +0100

    backup overwritten code

--- classifiers/binaryclassification.py
+++ classifiers/binaryclassification.py
@@ -19,6 +19,9 @@ from sklearn.svm import SVC
 from sklearn.neural_network import MLPClassifier
 from collections import defaultdict
 import os
+import tensorflow as tf
+from tensorflow import keras
+
 os.environ["CUDA_VISIBLE_DEVICES"] = "-1"
 
 
@@ -176,17 +179,27 @@ def train_test_model_kfold(X, Y, batch_size = 25):
     results = cross_val_score(estimator, X, Y, cv=kfold)
     return (results.mean()*100, results.std()*100)
 
-def train_model(X_train, y_train, batch_size = 200):
-    estimator = KerasClassifier(build_fn=create_baseline, epochs=100, batch_size=batch_size, verbose=1)
-    estimator.fit(X_train, y_train)
+def train_model(X_train, y_train, model_name, batch_size = 200):
+    try:
+        estimator = keras.models.load_model('neuralnets/'+model_name+'.h5')
+        print(model_name, ' neural net loaded')
+    except:
+        estimator = create_baseline()
+        print('training neural net')
+        estimator.fit(X_train, y_train, epochs = 100, batch_size = batch_size, verbose = 1)
+        estimator.save('neuralnets/'+model_name+'.h5', save_format = 'h5')
+
     return estimator
 
-def train_model_use(X_train, y_train, batch_size = 200):
-    estimator = KerasClassifier(build_fn=single_layer_1024, epochs=100, batch_size=batch_size, verbose=1)
-    estimator.fit(X_train, y_train)
-    return estimator
-
-    return estimator
+def train_model_use(X_train, y_train, model_name, batch_size = 200):
+    try:
+        estimator = keras.models.load_model('neuralnets/'+model_name+'.h5')
+        print(model_name, ' neural net loaded')
+    except:
+        estimator = single_layer_1024()
+        print('training neural net')
+        estimator.fit(X_train, y_train, epochs = 100, batch_size = batch_size, verbose = 1)
+        estimator.save('neuralnets/'+model_name+'.h5', save_format = 'h5')
 
 def split_train_test_vect(X, Y, partition_size = 0.7):
     partition = floor(len(Y)*partition_size)
@@ -199,12 +212,11 @@ def split_train_test_vect(X, Y, partition_size = 0.7):
     
 
 def test_model(X_test, y_test, model):
+    # print(model.predict_classes(X_test))
+    # return model.evaluate(X_test, y_test)
     y_pred = (model.predict(X_test) > 0.5).astype("int32")
 
-    print("Accuracy: ", accuracy_score(y_test, y_pred))
-    print("Precision: ", precision_score(y_test, y_pred))
-    print("Recall: ", recall_score(y_test, y_pred))
-    print("F1: ", f1_score(y_test, y_pred))
+    return accuracy_score(y_test, y_pred), precision_score(y_test, y_pred), recall_score(y_test, y_pred), f1_score(y_test, y_pred)
 
 def train_test_models(X_vectorized, Y, method, models = ['create_baseline'], batch_size = 200):
     accuracies = defaultdict(dict)
@@ -224,4 +236,5 @@ def train_test_models(X_vectorized, Y, method, models = ['create_baseline'], bat
     return accuracies
 
 if __name__ == "__main__":
-    pass
+    estimator = keras.models.load_model('neuralnets/w2v.h5')
+    
