136a5be4ef9fa1cbac9eff739499ece72699e275
C. Kattenberg <celine.kattenberg@student.uva.nl>
2021-01-19 21:49:44 +0100

    doc2vec hyperparameter tuning

--- embed/doc2vec_usage_ex.py
+++ embed/doc2vec_usage_ex.py
@@ -6,5 +6,6 @@ training_data = preprocess.clean_process(data)
 doc2vec.create_model(training_data, "models/doc2vec.model")
 model = doc2vec.load_model("models/doc2vec.model")
 embedding = doc2vec.doc2vec(model, ["The quick brown fox jumps over the lazy dog."])
+embedding2 = doc2vec.doc2vec(model, ["The quick brown fox jumps over the lazy dog."])
 
 print(embedding)
